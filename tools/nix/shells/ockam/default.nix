edf807c67c82df60adb4c53e3a2aecbc0782fb84
Mrinal Wadhwa <mrinal@ockam.io>
2023-07-01 10:43:30 -0700

    a

---
+++ tools/nix/shells/ockam/default.nix
@@ -0,0 +1,31 @@
+{
+  gnumake,
+  mkShell,
+}:
+#
+# Create a development shell using three sections:
+# `packages`, `buildInputs`, and `shellHook`.
+#
+mkShell {
+  # Compilers and libraries go here
+  buildInputs = [
+  ];
+
+  # Add extra tools here
+  packages = [
+    gnumake
+  ];
+
+  # Any variable set in this block that isn't a reserved word will be set as an
+  # environment variable in the environment.
+  WELCOME_MESSAGE = "Run make to build this project";
+
+  # A shell hook is a script to run when entering an environment.
+  # It can be used to perform any custom activation steps needed for your
+  # project.
+  shellHook = ''
+    make --version
+    echo
+    echo "$WELCOME_MESSAGE"
+  '';
+}
